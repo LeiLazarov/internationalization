6b96a5a2c4118814c30eeb5a73623d272f23434c
leilazarov <stoneiiilei@gmail.com>
2020-09-16 14:34:23 +1000

    minor improvement

--- myApp/views.py
+++ myApp/views.py
@@ -4,7 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 
 # Create your views here.
 def index(request):
-    print(request.LANGUAGE_CODE)
     context = {'msg': _("Welcome to China")}
     return render(request, 'myApp/index.html', context)
 
